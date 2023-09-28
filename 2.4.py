b1ba5c9893c18f7d52ca889523f856eb78a4ca22
Vlrrn <majviasna@gmail.com>
2023-09-28 20:03:36 +0300

    2.4 try\except\finall

--- 2.4.py
+++ 2.4.py
@@ -1,3 +1,19 @@
 #                    Задание 4
 # Напишите программу, демонстрирующую работу try\except\finally
 
+print("Задание 4\n")
+while True:
+    try:
+        a = float(input("Введите делимое: "))
+        b = float(input("Введите делитель: "))
+        c = a / b
+        print("Частное: %.2f" % c)
+    except ValueError:
+        print("Нельзя вводить строки")
+    except ZeroDivisionError:
+        print("Нельзя делить на ноль")
+    else:
+        print("Конец")
+        break
+    finally:
+        print("-------")
