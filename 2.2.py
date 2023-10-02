e6762dc4c211bb1eb5eab1acac1bdc681f17b254
Vlrrn <majviasna@gmail.com>
2023-10-02 19:54:13 +0300

    2.2 function, color

--- 2.2.py
+++ 2.2.py
@@ -6,14 +6,8 @@
 #   -Если список, то найти произведение между первым и вторым отрицательными элементами.
 #       Максимальный и минимальный элемент поменять местами.
 # Сделать проверку со всеми этими случаями
-
-def check_e(s):
-    while True:
-        try:
-            el = int(input(s))
-            return el
-        except ValueError:
-            print("Введите целое число!")
+def color_text(text):
+    print("\033[1m\033[36m{}\033[0m".format(text))
 
 
 def check_k(s):
@@ -21,20 +15,11 @@ def check_k(s):
         try:
             k_e = int(input(s))
             if k_e <= 0:
-                print("!?")
+                print("\033[1m\033[31m{}\033[0m".format("\nКоличество должно быть положительным!"))
                 continue
             return k_e
         except ValueError:
-            print("Введите целое число!")
-
-
-def check_i(s):
-    while True:
-        try:
-            c = int(input(s))
-            return c
-        except ValueError:
-            print("Введите целое число!")
+            print("\033[1m\033[31m{}\033[0m".format("\nВведите целое положительное число!"))
 
 
 def check_int(s):
@@ -43,39 +28,36 @@ def check_int(s):
             c = int(input(s))
             return c
         except ValueError:
-            print("Введите целое число!")
+            print("\033[1m\033[31m{}\033[0m".format("\nВведите целое число!"))
 
 
 def function(arg):
     print()
     if type(arg) == str:
-        print("str")
         arg = arg.split()
         if len(arg) > 0:
             a = max(arg, key=len)
-            print(a)
-            print(len(a))
+            print("Самое длинное слово: ", a)
+            print("Его длина =", len(a))
         else:
-            print("Не найдено слов")
+            print("Не найдено слов.")
     elif type(arg) == int:
-        print("int")
         summa = 0
         str_i = str(arg)
-        print(str_i)
+        print("Число: ", str_i)
         str_i = str_i.replace('-', '')
-        print(str_i)
+        # print(str_i)
         for i in str_i:
             if '0' <= i <= "9":
                 summa += int(i)
-        print(summa)
+        print("Сумма цифр числа = ", summa)
     elif type(arg) == set:
-        print("set")
+        print("Множество: ", arg)
         summ = 0
         for i_set in arg:
             summ += i_set
-        print(summ)
+        print("Сумма элементов =", summ)
     elif type(arg) == list:
-        print("list")
         first_min = second_min = -1
         for i in range(0, len(arg)):
             if arg[i] < 0:
@@ -89,9 +71,9 @@ def function(arg):
                 s *= arg[i]
             print(s)
         elif first_min == second_min - 1:
-            print("0 elements")
+            print("Отрицательные элементы находятся рядом.")
         else:
-            print("Nothing to do")
+            print("Нет двух отрицательных элементов.")
         min_el = max_el = arg[0]
         min_el_i = max_el_i = arg.index(min_el)
         for i in arg:
@@ -103,55 +85,81 @@ def function(arg):
                 max_el_i = arg.index(max_el)
         arg[min_el_i] = max_el
         arg[max_el_i] = min_el
-        print(arg)
+        print("\nMax и Min поменялись местами.\nНовый список:", arg)
 
 
 while True:
     print()
-    print("Что вы хотите ввести?\n" +
-          "***********************\n" +
-          "     1. Строку\n" +
-          "     2. Число\n" +
-          "     3. Множество\n" +
-          "     4. Список\n" +
-          "     0 - ВЫХОД\n" +
-          "***********************")
+    print("\033[3m\033[36m{}\033[0m".format("""
+     Что вы хотите ввести?
+    ***********************
+         1. Строку
+         2. Число
+         3. Множество
+         4. Список
+         0 - ВЫХОД
+    ***********************"""))
     while True:
-        v = check_i("- ")
+        v = check_int("- ")
         if v < 0 or v > 4:
-            print("Такого пункта нет. Повторите ввод")
+            print("\nТакого пункта нет. Повторите ввод:")
         else:
             break
     print()
     if v == 1:
-        st = input("""Введите строку, состоящую из слов 
-                    (другие символы будут удалены): """)
+        color_text("""
+=======================================
+        ВЫ ВЫБРАЛИ ВВОД СТРОКИ
+=======================================""")
+        st = input("""
+Введите строку, состоящую из слов 
+(другие символы будут удалены)
+- """)
         import re
         new_st = re.sub("[^A-Za-z ]", "", st)
-        print(new_st)
+        print("\nСтрока преобразована:\n" +
+              "-", new_st)
         function(new_st)
     elif v == 2:
         print()
-        f = check_int("int ")
+        color_text("""
+=======================================
+        ВЫ ВЫБРАЛИ ВВОД ЧИСЛА
+=======================================""")
+        f = check_int("""
+Введите число.
+(может быть <0)
+- """)
         function(f)
     elif v == 3 or v == 4:
-        k = check_k("Количество элементов = ")
+        if v == 3:
+            color_text("""
+=======================================
+      ВЫ ВЫБРАЛИ ВВОД МНОЖЕСТВА
+=======================================""")
+        else:
+            color_text("""
+=======================================
+        ВЫ ВЫБРАЛИ ВВОД СПИСКА
+=======================================""")
+        k = check_k("\nКоличество элементов = ")
+        print()
         if v == 3:
             mn = set()
             i = 1
             while k > len(mn):
-                mn.add(check_e("Введите %.f элемент: " % i))
+                mn.add(check_int("Введите %.f элемент: " % i))
                 i += 1
             function(mn)
         else:
             l = []
             i = 1
             while k > len(l):
-                l.append(check_e("Введите %.f элемент: " % i))
+                l.append(check_int("Введите %.f элемент: " % i))
                 i += 1
             function(l)
     elif v == 0:
-        print("""
+        print("\033[33m{}\033[0m".format("""
                 ／＞　    フ
         　　　　　| 　_　 _|
         　 　　　／`ミ _x 彡
@@ -160,5 +168,5 @@ while True:
         　／￣|　　 |　|　|
         　| (￣ヽ＿_ヽ_)_)
         　＼二つ
-        """)
+**********************************"""))
         exit()
